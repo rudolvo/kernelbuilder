0958a3bf4f10f3e81cb36410d434fe574b8069ee
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-28 11:44:39 +0700

    Add files via upload

---
+++ buildscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget https://android.googlesource.com/kernel/common/+archive/34b5f809f17e66d5011086a3d90802989e667f75.tar.gz
+gunzip 34b5f809f17e66d5011086a3d90802989e667f75.tar.gz
+mkdir kernel && cd kernel
+tar -xz ../34b5f809f17e66d5011086a3d90802989e667f75
+sudo apt update
+sudo apt install libssl-dev clang llvm gcc-aarch64-linux-gnu bc -y
+mkdir -p out
+make clean
+make mrproper
+make O=out ARCH=arm64 gki_defconfig
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+					  
+zip -r out out
