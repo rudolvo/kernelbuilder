f069d19014f9ef052a22c1dcfd2618c9c9032ee6
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-28 15:04:19 +0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-curl https://android.googlesource.com/kernel/common/+archive/34b5f809f17e66d5011086a3d90802989e667f75.tar.gz -o compr.tar.gz > /dev/null 2>&1
 mkdir kernel && cd kernel
-tar -xzf ../compr.tar.gz
+tar -xzf ../source.tar.gz
 sudo apt update > /dev/null 2>&1
 sudo apt install libssl-dev clang llvm gcc-aarch64-linux-gnu bc -y
 mkdir -p out
