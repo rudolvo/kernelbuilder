7a76f8e7dce3d4b1bbf1099c3c6c81665d0f0dea
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-28 14:55:18 +0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl https://android.googlesource.com/kernel/common/+archive/34b5f809f17e66d5011086a3d90802989e667f75.tar.gz -o compr.tar.gz > /dev/null
+curl https://android.googlesource.com/kernel/common/+archive/34b5f809f17e66d5011086a3d90802989e667f75.tar.gz -o compr.tar.gz > /dev/null 2>&1
 mkdir kernel && cd kernel
 tar -xzf ../compr.tar.gz
-sudo apt update
+sudo apt update > /dev/null 2>&1
 sudo apt install libssl-dev clang llvm gcc-aarch64-linux-gnu bc -y
 mkdir -p out
 make clean
@@ -14,5 +14,5 @@ make -j$(nproc --all) O=out \
 		      LLVM_IAS=0 \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi-
-zip -r root.zip arch/arm64 > /dev/null
-zip -r out.zip out/arch/arm64 > /dev/null				  
+zip -r root.zip arch/arm64 > /dev/null 2>&1
+zip -r out.zip out/arch/arm64 > /dev/null 2>&1				  
