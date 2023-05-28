72a8e5850eea684c5ac5b1ad2a689ca81cf4e7e4
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-28 15:36:57 +0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -6,16 +6,16 @@ echo "Decompressing source code..."
 tar -xzf ../compr.tar.gz
 echo "Preparing build tools"
 sudo apt update > /dev/null 2>&1
-sudo apt install libssl-dev clang llvm gcc-aarch64-linux-gnu bc -y
+sudo apt install libssl-dev clang ccache gcc-aarch64-linux-gnu bc -y
 mkdir -p out
 echo "Starting make..."
 make clean
 make mrproper
 make O=out ARCH=arm64 gki_defconfig
+ccache -M 50G
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-		      LLVM=1 \
-		      LLVM_IAS=0 \
+		      CC="/usr/bin/ccache clang" \
 		      CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi-
