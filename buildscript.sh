a4adf3c873529b2f669d9463d631add896fbad58
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-29 11:44:06 +0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -5,16 +5,17 @@ echo "Decompressing source code..."
 mv kernel_xiaomi_sm6375 kernel && cd kernel
 echo "Preparing build tools"
 sudo apt update > /dev/null 2>&1
-sudo apt install build-essential gcc-aarch64-linux-gnu gperf lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev lzop clang schedtool ccache rsync -y
+sudo apt install build-essential gcc-aarch64-linux-gnu gperf lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev llvm lzop clang schedtool ccache rsync -y
 mkdir -p out
 echo "Starting make..."
 make clean
 make mrproper
-make O=out ARCH=arm64 gki_defconfig
+make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- gki_defconfig
 ccache -M 50G
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
 		      CC="/usr/bin/ccache clang" \
+		      LLVM=1 \
 		      CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-androideabi-
