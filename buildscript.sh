da2da961fa2399819de8f15e3c75463f8686e294
katyusha128 <70309181+katyusha128@users.noreply.github.com>
2023-05-30 19:36:26 +0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -3,10 +3,13 @@ git config --global user.email "nhat.dogpro@outlook.com"
 git config --global user.name "katyusha256"
 mkdir kernel && cd kernel
 
+sudo apt update
+sudo apt install repo git
+
 repo init -u https://android.googlesource.com/kernel/manifest
 mv ../manifest.xml .repo/manifests
 repo init -m manifest.xml
-#repo sync
+repo sync
 
 cd ..
 git add ./kernel/*
