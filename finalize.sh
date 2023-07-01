c2624f636c2dcd078cba2ad7a45dfb00b088846a
cachiusa <70309181+cachiusa@users.noreply.github.com>
2023-07-01 09:55:33 +0000

    changed a lot [skip ci]

--- finalize.sh
+++ finalize.sh
@@ -3,8 +3,9 @@ if grep -xq "Installing UAPI kernel headers:" build.log
 then
     echo Adding Image to AnyKernel flashable zip...
     git clone https://github.com/karthik558/AnyKernel3
-    
-    zip -r9 "$ZIPNAME" * -x .git README.md anykernel-real.sh .gitignore zipsigner* *.zip
+    cp ./out/android11-5.4/dist/Image ./AnyKernel3
+    cd AnyKernel3
+    zip -r9 "update.zip" * -x .git README.md anykernel-real.sh .gitignore zipsigner* *.zip
 else
     echo Build failed. Exiting...
     exit 2
