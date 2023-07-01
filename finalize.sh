d26f2c1881caa8faea66c348c19d02d68c661e12
cachiusa <70309181+cachiusa@users.noreply.github.com>
2023-07-02 00:47:01 +0700

    fixed some more bs [no ci]

--- finalize.sh
+++ finalize.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if grep -q "Files copied to" build.log
 then
+    mv out/android11-5.4/dist/Image AnyKernel3
+    mv build.log AnyKernel3
     echo Workflow will take care the zip file.
 else
     echo Build ended prematurely. Exiting...
