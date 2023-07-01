7169385cb9180d6e0d3cebd3453789addca58b67
cachiusa <70309181+cachiusa@users.noreply.github.com>
2023-07-01 21:27:59 +0700

    forgor

--- startbuild.sh
+++ startbuild.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo Available threads: $(nproc)
 echo .
-BUILD_CONFIG=common/build.config.veux build/build.sh 2>&1 | tee build.log
+if grep -q "V=1" common/build.config.veux
+then
+    #Prevent console flooding in verbose mode
+    BUILD_CONFIG=common/build.config.veux build/build.sh > build.log 2>&1
+else
+    BUILD_CONFIG=common/build.config.veux build/build.sh 2>&1 | tee build.log
+fi
